f1e0afc5996441a233dffd50ccd53944a8b87a95
maverick12-S <kzuuki14148@gmail.com>
2024-12-28 10:43:44 +0900

    Level1を完了/Level2のページ作成

---
+++ src/lv2/LevelTwo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LevelTwo = () => {
+    return(
+        <div>
+            <h1>Level 2 Page</h1>
+            <p>リソースサーバーのDBからRestApiを用いてデータ取得</p>
+        </div>
+    );
+};
+
+export default LevelTwo;
